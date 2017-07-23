beb2afa7c6530369154374009b3fc78c33fe24ed
AndrewT78 <andrew.m.tyler@gmail.com>
2017-07-23 21:30:58 +0800

    Moved  GPS code into station

--- index.android.js
+++ index.android.js
@@ -16,55 +16,23 @@ import {
 import Station from './station.js'; 
 
 
-
-
 export default class GetMeHome extends Component {
   
    constructor(props) {
     super(props);
-
-    this.state = {
-      latitude: null,
-      longitude: null,
-      error: null,
-    };
   }
   
-  componentDidMount() {
-
-    this.setState({ error: "Waiting..." });
-
-   this.watchId = navigator.geolocation.watchPosition(
-      (position) => {
-        this.setState({
-          latitude: position.coords.latitude,
-          longitude: position.coords.longitude,
-          error: null,
-        });
-      },
-      (error) => this.setState({ error: error.message }),
-      { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000, distanceFilter: 10 },
-    );
-  }
-
-  componentWillUnmount() {
-    navigator.geolocation.clearWatch(this.watchId);
-  }
 
   render() {
-    return (
+    return (       
       <View style={styles.container}>
         <Text style={styles.welcome}>
           Get Me Home!
         </Text>
-        <Text>Latitude: {this.state.latitude}</Text>
-         <Text>Longitude: {this.state.longitude}</Text>      
-         <Text>GPS Error: {this.state.error}</Text>
-        
+                
         <Text style={styles.instructions}>
           Select your destination...
-        </Text>  
-             
+        </Text>               
         <Station name='Glendalough' />      
         <Station name='Stirling' />      
         <Station name='Warwick' />      
--- station.js
+++ station.js
@@ -2,27 +2,54 @@ import React, { Component } from 'react';
 import { AppRegistry, Text, View, StyleSheet, TouchableHighlight } from 'react-native';
 export default class Station extends Component {
 constructor(props) {
-super(props);
-this.state = {pressed: false};
-// Toggle the state every second
-//setInterval(() => {
- //this.setState(previousState => {
- //return { pressed: !previousState.pressed };
- //});
- //}, 1000);
+  super(props);
+  
+    this.state = {
+      latitude: null,
+      longitude: null,
+      error: null,
+      pressed: false
+    };
+
 }
 render() {
-return (
-<TouchableHighlight style={[stationStyles.station, this.state.pressed && stationStyles.selected]} onPress={() => this.setState({ pressed: !this.state.pressed })}>
-<View>
-<Text style={[stationStyles.stationName, this.state.pressed && stationStyles.stationNameSelected]}>{this.props.name}</Text>
-{this.state.pressed &&
-  <Text>Rest easy now....you will be woken...</Text>   
+  return (
+  <TouchableHighlight style={[stationStyles.station, this.state.pressed && stationStyles.selected]} onPress={() => this.setState({ pressed: !this.state.pressed })}>
+  <View>
+  <Text style={[stationStyles.stationName, this.state.pressed && stationStyles.stationNameSelected]}>{this.props.name}</Text>
+  {this.state.pressed &&
+    <View>
+    <Text>Latitude: {this.state.latitude}</Text>
+         <Text>Longitude: {this.state.longitude}</Text>      
+         <Text>GPS Error: {this.state.error}</Text>   
+         </View>
+  }
+  </View> 
+  </TouchableHighlight>
+  );
 }
-</View> 
-</TouchableHighlight>
-);
-}
+
+ componentDidMount() {
+
+    this.setState({ error: "Waiting..." });
+
+   this.watchId = navigator.geolocation.watchPosition(
+      (position) => {
+        this.setState({
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude,
+          error: null,
+        });
+      },
+      (error) => this.setState({ error: error.message }),
+      { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000, distanceFilter: 10 },
+    );
+  }
+
+  componentWillUnmount() {
+    navigator.geolocation.clearWatch(this.watchId);
+  }
+
 }
 const stationStyles = StyleSheet.create({
 station: {
